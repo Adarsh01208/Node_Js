69d477d4d977b5d73065b2e5d17ef1c35cc42d57
Adarsh01208 <adarshsingh01209@gmail.com>
2024-01-20 16:21:57 +0530

    Added new feature

--- contact_list/config/mongoose.js
+++ contact_list/config/mongoose.js
@@ -4,18 +4,17 @@ const mongoose = require('mongoose');
  mongoose.connect('mongodb://localhost/contact_list_db');
 
 
-
 //acquire the connection(to check if it is successful)
 const db = mongoose.connection;
 
 //error
 db.on('error', console.error.bind(console, 'error connecting to db'));
 
-
-
-
 //up and running then print the message
 db.once('open', function () {
     console.log('Successfully connected to database');
 });
 
+module.exports = db;
+
+//ending the connection
