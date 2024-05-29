7ac018f8f18d3d6c99ea0e3aab0ed7f8a82c731c
Adarsh01208 <adarshsingh01209@gmail.com>
2024-05-29 16:12:46 +0530

    chore: Refactor REST API endpoints for user CRUD operations

--- Rest_api/index.js
+++ Rest_api/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const data = require('./MOCK_DATA.json')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8000
+app.use(bodyParser.json())
 
 
 // hybride api for all users
-app.get('/api/users', (req,res)=>{
+app.get('/api/users', (req, res) => {
     res.json(data)
 })
 // html rendring api for all users
@@ -23,6 +25,9 @@ app.get('/users', (req, res) => {
 app.get('/api/users/:id', (req, res) => {
     const id = req.params.id
     const user = data.find(user => user.id == id)
+    if (!user) {
+        res.status(404).send('User not found')
+    }
     res.json(user)
 })
 
@@ -30,17 +35,27 @@ app.get('/api/users/:id', (req, res) => {
 app.post('/api/users', (req, res) => {
     const user = req.body
     data.push(user)
-    res.send('User has been added')
+    res.send(data)
+    console.log("User added successfully")
 })
 
 // edit the user with id
 app.patch('/api/users/:id', (req, res) => {
-    const  id = req.params.id
+    const id = req.params.id
     const user = data.find(user => user.id == id)
     if (!user) {
         res.status(404).send('User not found')
     }
-    res.send(`User with id ${id} has been updated`)
+    const newUser = req.body
+    data.forEach(user => {
+        if (user.id == id) {
+            user.first_name = newUser.first_name
+            user.last_name = newUser.last_name
+            user.email = newUser.email
+        }
+    })
+    console.log("User updated successfully")
+    res.json(data)
 })
 
 // delete the user with id
@@ -50,15 +65,15 @@ app.delete('/api/users/:id', (req, res) => {
     if (!user) {
         res.status(404).send('User not found')
     }
-    newdata = data.filter(user => user.id != id)
-    //res.json(newdata)
-    res.send(`User with id ${id} has been deleted successfully`);
+    data.splice(data.indexOf(user), 1)
+    console.log("User deleted successfully")
+    res.send(`User with id ${id} has been deleted`)
 })
 
 
 
-app.listen(port, (err)=> {
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log(err)
     }
     console.log(`Server is running on port ${port}`)
