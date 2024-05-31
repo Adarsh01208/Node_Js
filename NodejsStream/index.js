04f1bde61d9ef65cf1a11c5eb57934374089d3ec
Adarsh01208 <adarshsingh01209@gmail.com>
2024-06-01 00:48:26 +0530

    chore: Refactor file reading in Node.js stream and add compression

--- NodejsStream/index.js
+++ NodejsStream/index.js
@@ -2,19 +2,20 @@ const express = require("express");
 const fs = require("fs");
 const status = require("express-status-monitor");
 const app = express();
+const zlib = require("zlib");
 
 app.use(status())
-// using readfile method it uses lot of memory
-// app.get("/", (req, res) => {
-//     fs.readFile("sample.txt", "utf8", (err, data) => {
-//         if (err) {
-//             console.error(err);
-//             return;
-//         }
-//         res.send(data);
-//     }
-//     );
-// });
+// using readfile method it uses more memory
+app.get("/", (req, res) => {
+    fs.readFile("sample.txt", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+    }
+    );
+});
 
 // using readstream method it uses less memory
 app.get("/", (req, res) => {
@@ -22,6 +23,21 @@ app.get("/", (req, res) => {
     readStream.on("open", () => {
         readStream.pipe(res);
     });
+    readStream.on("error", (err) => {
+        console.error(err);
+        res.end(err);
+    });
+});
+
+// using readstream method it uses less memory and compress the file
+app.get("/", (req, res) => {
+    const readStream = fs.createReadStream("sample.txt", "utf8");
+
+    // Compress the file with zlib and pipe it to the response
+    readStream
+        .pipe(zlib.createGzip())
+        .pipe(res);
+
     readStream.on("error", (err) => {
         console.error(err);
         res.end(err);
