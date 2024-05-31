8d8c3a98a4283b67faebeaf140b09babe6a099d5
Adarsh01208 <adarshsingh01209@gmail.com>
2024-05-31 16:33:10 +0530

    chore: Add multer file upload functionality

---
+++ multer_fileupload/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const multer = require('multer')
+
+const path = require('path')
+const port = 8000
+const app = express()
+ 
+const upload = require('./multer')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+
+// uploadinf file using multer
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+app.post('/upload', upload.single('avatar'), (req, res) => {
+   console.log(req.body)
+    console.log(req.file)
+    res.send('File uploaded successfully')
+})
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Error in running the server', err)
+    }
+    console.log(`Server is running on port ${port}`)
+})
+
